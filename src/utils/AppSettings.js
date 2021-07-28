99ef5ff57bab73cbbf81c42507f7226af42d16b7
barendan <barendan@gmail.com>
2021-07-28 16:13:21 -0400

    configure basic app  and network settings

---
+++ src/utils/AppSettings.js
@@ -0,0 +1,9 @@
+const appKey = 'Dos-EHNCASG';
+const appVersion = 'v1.0';
+const buildVersion = '0621';
+
+export default {
+  appKey,
+  appVersion,
+  buildVersion,
+};
---
+++ src/utils/NetworkSettings.js
@@ -0,0 +1,16 @@
+// DEV
+// export const fullApiURl = ``;
+// export const geoLocationAPI = '';
+
+// PROD
+const apiBaseEndpointVersion = 'v1';
+const fullApiURL = 'https://integratedservices.dos.net/dosmobile/api/';
+const tokenAuthPath = 'oauth/token';
+const googleGeoCodeAddressURL = '';
+
+export default {
+  apiBaseEndpointVersion,
+  fullApiURL,
+  tokenAuthPath,
+  googleGeoCodeAddressURL,
+};
